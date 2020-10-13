767ad1514b332a9a47185bf82409712d3c06be36
Lachlan Ting <18lt01@gmail.com>
2020-10-13 13:53:41 +1100

    form tester

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,20 +1,11 @@
 <?php
-    $name = $_POST['name']; 
-    $email = $_POST['email']; 
-    $message = $_POST['message']; 
-
-    $email_from = 'Queries@csesocelections2021.com'; 
-    $email_subject = "Query"; 
-
-    $email_body = "Name: $name\n".
-                  "From: $email\n".
-                  "Message: $message\n"; 
-    
-    $to = "18lt01@gmail.com";
-
-    $headers = "From: $email_from \r\n"; 
-    $headers .= "Reply to: $email \r\n"; 
-    mail($to,$email_subject,$email_body, $headers); 
-    header("Location: index.html"); 
-
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "18lt01@gmail.com";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!";
 ?>
