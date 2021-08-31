e251eca08c59f8dd05feed83b1592e1579862333
Lachlan Ting <18lt01@gmail.com>
2021-08-31 14:36:54 +1000

    rearranged repo structure, changed text in heroSection

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,20 +7,20 @@ function Herosection() {
     return (
         <div className='hero-container'>
             <video src="/videos/video-2.mp4" autoPlay loop muted />
-            <h1>Adventure Awaits!</h1>
-            <p>lol</p>
+            <h1>Header 1!</h1>
+            <p>Point</p>
             <div className="hero-btns">
                 <Button 
                     className='btns' 
                     buttonStyle='btn--outline'
                     buttonSize='btn--large'> 
-                GET STARTED 
+                Button 1
                 </Button>
                 <Button 
                     className='btns' 
                     buttonStyle='btn--primary'
                     buttonSize='btn--large'>
-                WATCH trailer 
+                Button 2 
                 <i className='far fa-play-circle'/> 
                 </Button>
             </div>
