19bf1b32b71840932ee2a4a7410adac92cbc0d9d
Lachlan Ting <18lt01@gmail.com>
2021-09-01 23:59:43 +1000

    added pages to home page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import NavigationBar from './components/NavigationBar';
 import Home from './pages/Home'
+import Cards from './components/Cards';
+import AboutMe from './pages/About-Me';
+import Projects from './pages/Projects';
+import Blog from './pages/Blog';
 
 function App() {
   return (
@@ -11,6 +15,10 @@ function App() {
       <NavigationBar/>
       <Switch>
         <Route path='/' exact component={Home}/>
+        <Route path='/About-Me' exact component={AboutMe}/>
+        <Route path='/Projects' exact component={Projects}/>
+        <Route path='/Blog' exact component={Blog}/>
+        <Cards/>
       </Switch>
     </Router>
     </>
