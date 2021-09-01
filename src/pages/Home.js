7afc3d936da3bb236cd060544567be82bc5e0267
Lachlan Ting <18lt01@gmail.com>
2021-09-01 23:55:39 +1000

    added cards and footer

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import '../App.css';
+import Cards from '../components/Cards';
+import Footer from '../components/Footer';
 import HeroSection from '../components/HeroSection';
 
 function Home() {
     return(
         <>
             <HeroSection />
+            <Cards />
+            <Footer />
         </>
     );
 }
