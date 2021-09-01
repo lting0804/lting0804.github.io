97f0156c46a9c6d5b47f1f9022b71dfd9cd6c12c
Lachlan Ting <18lt01@gmail.com>
2021-09-01 23:58:58 +1000

    finished basic structure for Cards and CardItems

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,11 +1,39 @@
 import React from 'react'
+import CardItem from './CardItem'
+import './Cards.css'
 
 function Cards() {
     return (
-        <div>
+        <div className='cards'>
+            <h1> Check out some of my Projects!</h1>
+            <div className='cards__container'> 
+                <div className='cards__wrapper'> 
+                    <ul className='cards__items'>
+                        <CardItem
+                            src=''/*TO DO: PUT SS*/
+                            text='The makings of this very own website!'
+                            label='Web Development'
+                            path='/Projects/Website'
+                        />
+                        <CardItem
+                            src=''/*TO DO: PUT SS*/
+                            text='Implementation of Tensorflow for Machine Learning!'
+                            label='Machine Learning'
+                            path='/Projects/Machine-Learning'
+                        />
+                        <CardItem
+                            src=''/*TO DO: PUT SS*/
+                            text='My Own Take on a JS13K Game!'
+                            label='Web Development'
+                            path='/Projects/JS-Game'
+                        />
+                    </ul>
+                </div>
+            </div>
             
         </div>
     )
 }
 
 export default Cards
+
