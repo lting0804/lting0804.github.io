50ba66d287103fd0842ea4ed6f87f8ee57b8947c
Lachlan Ting <18lt01@gmail.com>
2021-09-02 18:33:41 +1000

    added basic structure for error 404

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import NavigationBar from './components/NavigationBar';
 import Home from './pages/Home'
-import Cards from './components/Cards';
 import AboutMe from './pages/About-Me';
 import Projects from './pages/Projects';
 import Blog from './pages/Blog';
+import NotFound from './pages/NotFound.js';
 
 function App() {
   return (
@@ -18,7 +18,7 @@ function App() {
         <Route path='/About-Me' exact component={AboutMe}/>
         <Route path='/Projects' exact component={Projects}/>
         <Route path='/Blog' exact component={Blog}/>
-        <Cards/>
+        <Route component={NotFound}/>
       </Switch>
     </Router>
     </>
---
+++ src/pages/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../App.css'; 
+
+const NotFound = () => {
+    return (
+        <div className='not-found-container'>
+            <h1> 404 - Not Found </h1>
+            <p> It seems like you have gotten lost!</p>
+        </div>
+  );
+};
+
+export default NotFound
